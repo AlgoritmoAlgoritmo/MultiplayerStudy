05039057ed7bf88c3de370ce3b6e6292d46eafae
Iris <31053695+AlgoritmoAlgoritmo@users.noreply.github.com>
2025-02-05 21:44:19 -0300

    Initial commit

---
+++ Netcode/Assets/Code/View/NetworkUIController.cs
@@ -0,0 +1,26 @@
+/*
+* Author: Iris Bermudez
+* GitHub: https://github.com/AlgoritmoAlgoritmo
+* Date: DD/MM/YYYY (DD/MM/YYYY)
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NetworkUIController : MonoBehaviour {
+	#region Variables
+	[SerializeField]
+	private Button serverButton;
+	[SerializeField]
+	private Button hostButton;
+	[SerializeField]
+	private Button clientButton;
+	#endregion
+
+	#region Public methods
+	#endregion
+}
